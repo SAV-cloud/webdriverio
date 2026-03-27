fa65909addfad84384a68c5726d9147d62268491
Andrii Suvala <andriisuvala@gmail.com>
2026-03-27 15:08:52 +0200

    added new report

--- test/tests/login.ddt.js
+++ test/tests/login.ddt.js
@@ -1,17 +1,17 @@
-// const loginPage = require('../pages/login.page');
-// const users = require('../data/users');
-// const EXPECTED_TITLE = 'Swag Labs';
+const loginPage = require('../pages/login.page');
+const users = require('../data/users');
+const EXPECTED_TITLE = 'Swag Labs';
 
-// describe('Login Page Feature', () => {
-//     it('should successfully login with valid credentials', async () => {
-//         await loginPage.open();
-//         await loginPage.login(users[0].username, users[0].password);
-//         await expect(browser).toHaveTitle(EXPECTED_TITLE);
-//     });
+describe('Login Page Feature', () => {
+    it('should successfully login with valid credentials', async () => {
+        await loginPage.open();
+        await loginPage.login(users[0].username, users[0].password);
+        await expect(browser).toHaveTitle(EXPECTED_TITLE);
+    });
 
-//     it('should show an error with invalid credentials', async () => {
-//         await loginPage.open();
-//         await loginPage.login(users[1].username, users[1].password);
-//         expect(loginPage.errorMsg).toBeDisplayed();
-//     });
-// });
+    it('should show an error with invalid credentials', async () => {
+        await loginPage.open();
+        await loginPage.login(users[1].username, users[1].password);
+        expect(loginPage.errorMsg).toBeDisplayed();
+    });
+});
